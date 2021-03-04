ec0d9dd55974fb9b250a7dc8ac301c6480ea4e03
shimanaa <fshima02@gmail.com>
2021-03-04 21:10:34 +0330

    Revert "add accounts urls, views and base"

--- accounts/views.py
+++ accounts/views.py
@@ -4,53 +4,47 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.views import View
 
+# Create your views here.
 
-class RegisterUser(View):
-    form_class = RegisterForm
-    template = 'accounts/register.html'
-
-    def get(self, request):
-        form = self.form_class
-        return render(request, self.template, {'form':form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            user = User.objects.create_user(cd['username'], cd['email'], cd['password'])
-            login(request, user)
-            messages.success(request, "You're register in successfully", 'success')
-            return redirect('blog:index')
-        return render(request, self.template, {'form':form})
-
-
-
-class UserLogin(View):
-    form_class = LoginForm
-    template = 'accounts/login.html'
-
-    def get(self, request):
-        form = self.form_class
-        return render(request, self.template, {'form':form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 messages.success(request, "You're logged in successfully", 'success')
                 return redirect('blog:index')
-            messages.error(request, "Wrong username or password", 'danger')
-        return render(request, self.template, {'form':form})
+            
+            else:
+                messages.warning(request, "Wrong username or password", 'danger')
+    
+    else:
+        form = LoginForm()
+    
+    return render(request, 'accounts/login.html', {'form':form})
 
 
-class UserLogout(View):
-    def get(self, request):
-        logout(request)
-        messages.success(request, "logged out successfully", "success")
-        return redirect('blog:index')
 
+def user_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, "Your registeration complete", 'success')
+            return redirect('accounts:login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form':form})
+
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "logged out successfully", "success")
+    return redirect('blog:index')
+        
