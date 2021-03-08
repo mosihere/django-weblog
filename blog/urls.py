21bdadec52e0e69f1060766966de3d4fc46ea470
Mosihere <mostafakhoshhal@yahoo.com>
2021-03-08 20:01:19 +0330

    Fixing some bug

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.article_index, name='index'),
     path('<int:pk>/', views.article_detail, name='detail'),
-]
+] 
